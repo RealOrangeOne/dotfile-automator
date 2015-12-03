3dd1b8d7555ff8db50a723d29399e307dd2e9b9e
Jake Howard <git@theorangeone.net>
2015-12-03 18:18:04 +0000

    Added show command for config

--- project/config/cli.py
+++ project/config/cli.py
@@ -2,7 +2,14 @@ import click
 from project.utils import config
 
 
-@click.command('config')
-@click.argument('key')
-def cli(key):
+@click.group('config')
+def cli():
+    pass
+
+
+@cli.command('show')
+@click.argument('key', nargs=-1)
+def show(key):
+    key = " ".join(key)
     print(config.get(key))
+    return 0
